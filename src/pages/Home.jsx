1d3d8228f1509bb9782f682c375d0faee6ff78ce
mr siddhant dhakad <aakashdhakad@gmail.com>
2026-03-16 18:19:16 +0530

    init

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,24 @@
+import { Link } from 'react-router-dom';
+
 const Home = () => {
   return (
-    <section className="text-center py-10 bg-gray-50">
+    <div className="min-h-screen bg-white">
+      <div className="text-center py-10 bg-gray-50">
 
-      <p className="text-purple-500 mb-4">
-        🏆 "CTX Growth Champion Year 2025"
-      </p>
+        <p className="text-purple-500 mb-4">
+          🏆 "CTX Growth Champion Year 2025"
+        </p>
 
-      <h1 className="text-5xl font-bold mb-6">
-        <span className="text-green-500">5X Your Revenue</span> <br />
-        with the Power of WhatsApp
-      </h1>
+        <h1 className="text-5xl font-bold mb-6">
+          <span className="text-green-500">5X Your Revenue</span> <br />
+          with the Power of WhatsApp
+        </h1>
 
-      <p className="text-gray-600 max-w-2xl mx-auto mb-8">
-        Broadcast, Automate, Engage, Sell – do everything with the AI-powered
-        WhatsApp Marketing Platform.
-      </p>
+        <p className="text-gray-600 max-w-2xl mx-auto mb-8">
+          Broadcast, Automate, Engage, Sell – do everything with the AI-powered
+          WhatsApp Marketing Platform.
+        </p>
+      </div>
 
       <div className="flex justify-center gap-4">
         <button className="bg-green-500 text-white px-6 py-3 rounded-lg hover:bg-green-600">
@@ -26,7 +30,153 @@ const Home = () => {
         </button>
       </div>
 
-    </section>
+      <div className="mt-15 mb-15 text-center">
+
+        <h1 className="text-5xl mb-5 font-bold">Founders & Marketers Love us</h1>
+        <p className="font-semibold text-gray-600">Trusted by 150,000+ Businesses across 60+ Countries</p>
+
+      </div>
+
+      <div className="flex pl-10">
+        <div className="w-1/2 py-10 px-2">
+          <h1 className="text-5xl font-bold">Broadcast Promotional Messages on WhatsApp (Officially)</h1>
+          <p className=" text-gray-500 mt-4">
+            Enjoy a Limitless Broadcasting experience on WhatsApp
+          </p>
+          <div className="flex mb-3">
+            <div className="mr-3 mt-4">
+              <img
+                src="https://umsousercontent.com/lib_bylSIrVINmKpCoJQ/y402w4axyqwt3m3z.png?w=24&h=24&dpr=2"
+                alt="megaphone"
+                className="relative w-6 h-6 object-contain block"
+              />
+
+            </div>
+            <div className="mt-2">
+              <h1 className="text-lg font-semibold">
+                8+ Powerful Messaging Categories
+              </h1>
+              <p className="text-sm text-gray-500">
+                Send Promotions, Offers, Coupon codes, Carousels and More- Risk-Free!
+              </p>
+
+            </div>
+
+          </div>
+
+          <div className="flex mb-3">
+            <div className="mr-3 mt-4">
+              <img
+                src="https://umsousercontent.com/lib_bylSIrVINmKpCoJQ/1fieb932kqxiin4w.png?w=24&h=24&dpr=2"
+                alt="communication chat message"
+                className="relative w-6 h-6 block object-contain"
+              />
+
+            </div>
+            <div className="mt-2">
+              <h1 className="text-lg font-semibold">
+
+                Add CTAs. Drive 3x Conversions
+              </h1>
+              <p className="text-sm text-gray-500">
+                Turn conversations into conversions with eye-catching CTA and Quick Reply buttons
+              </p>
+
+            </div>
+
+          </div>
+
+          <div className="flex pt-2 mb-10">
+            <div className="mr-3 mt-4">
+              <img
+                src="https://umsousercontent.com/lib_bylSIrVINmKpCoJQ/b8h3rh4grkml4j5s.png?w=24&h=24&dpr=2"
+                alt="chat message conversation"
+                className="relative w-6 h-6 block object-contain"
+              />
+
+            </div>
+            <div className="mt-2">
+              <h1 className="text-lg font-semibold">
+                Schedule your WhatsApp messages
+              </h1>
+              <p className="text-sm text-gray-500">
+                Streamline your work, Schedule Broadcasts 2 months ahead of time
+              </p>
+
+            </div>
+
+          </div>
+          <Link to="#">
+            <button className="bg-green-400 hover:bg-green-500 text-white px-3 py-2 rounded flex items-center gap-2">
+              <span>Start For FREE</span>
+              <svg width="1em" viewBox="0 0 15 12" xmlns="http://www.w3.org/2000/svg" class="um-arrow"><path d="M9.6 7H1a1 1 0 1 1 0-2h8.6L7 2.4A1 1 0 0 1 8.4 1l4.3 4.2c.2.3.3.5.3.8 0 .3-.1.5-.3.7L8.4 11A1 1 0 1 1 7 9.5L9.6 7z" fill="currentColor"></path></svg>
+            </button>
+          </Link>
+        </div>
+        <div className="w-2/5">
+          <img
+            src="https://umsousercontent.com/lib_EyxlwrMuBuWXHRhZ/2cbv2iqtg8phy296.webp"
+            alt="Broadcast"
+            className="w-full h-auto object-contain"
+          />
+        </div>
+      </div>
+
+      <div className="flex ml-10 ">
+        <div className="w-1/2 py-10 px-2">
+          <h1 className="text-xl font-bold">Run AI powered Ads that Click to WhatsApp</h1>
+          <p className="text-gray-600 mt-1">
+            Run Ads on Facebook & Instagram that land on WhatsApp. 5X Your lead generations & 2-3X Conversions Instantly!
+          </p>
+          <p className="py-5 text-gray-600 mt-1">
+            Run Ads from AiSensy, get quality leads with AI & conversions API, smartly segregate your leads and build Chatbot Flows to automate everything!
+          </p>
+          <Link to="#">
+            <button className="bg-green-400 hover:bg-green-500 text-white px-3 py-2 rounded flex items-center gap-2">
+              <span>Explore</span>
+              <svg width="1em" viewBox="0 0 15 12" xmlns="http://www.w3.org/2000/svg" class="um-arrow"><path d="M9.6 7H1a1 1 0 1 1 0-2h8.6L7 2.4A1 1 0 0 1 8.4 1l4.3 4.2c.2.3.3.5.3.8 0 .3-.1.5-.3.7L8.4 11A1 1 0 1 1 7 9.5L9.6 7z" fill="currentColor"></path></svg>
+            </button>
+          </Link>
+        </div>
+        <div className="w-1/2">
+          <img
+            src="https://umsousercontent.com/lib_EyxlwrMuBuWXHRhZ/iamo9w4rxjqhx5pw.jpg?w=800&h=800&dpr=2"
+            alt="AiSensy illustration"
+            className="w-full h-auto object-contain"
+          />
+
+        </div>
+
+      </div>
+
+      <div className="flex ml-10 ">
+        <div className="w-1/2">
+          <img
+  src="https://umsousercontent.com/lib_EyxlwrMuBuWXHRhZ/9kysgifrjsd48evo.jpg?w=800&h=800&dpr=2"
+  alt="AiSensy illustration"
+  className="w-full h-auto object-contain"
+/>
+
+        </div>
+        <div className="w-1/2 py-10 px-2 m-auto ">
+          <h1 className="text-xl font-bold">Run AI powered Ads that Click to WhatsApp</h1>
+          <p className="text-gray-600 mt-1">
+           Capture Leads & collect useful information Directly in WhatsApp Chats with WhatsApp Forms.
+          </p>
+          <p className="py-5 text-gray-600 mt-1">
+           From feedback to gathering user insights, collect it all on WhatsApp.
+          </p>
+          <Link to="#">
+            <button className="bg-green-400 hover:bg-green-500 text-white px-3 py-2 rounded flex items-center gap-2">
+              <span>Explore</span>
+              <svg width="1em" viewBox="0 0 15 12" xmlns="http://www.w3.org/2000/svg" class="um-arrow"><path d="M9.6 7H1a1 1 0 1 1 0-2h8.6L7 2.4A1 1 0 0 1 8.4 1l4.3 4.2c.2.3.3.5.3.8 0 .3-.1.5-.3.7L8.4 11A1 1 0 1 1 7 9.5L9.6 7z" fill="currentColor"></path></svg>
+            </button>
+          </Link>
+        </div>
+        
+      </div>
+
+    </div>
   );
 };
 
